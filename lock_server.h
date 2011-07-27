809e86d7baa26be1fe7d0f90b07a6476ddd72b8c
naturegirl <jenny.guo@aurorafeint.com>
2011-07-27 21:36:31 +0800

    added code for lab1

--- lock_server.h
+++ lock_server.h
@@ -9,15 +9,35 @@
 #include "lock_client.h"
 #include "rpc.h"
 
+// Define this class to manage locks. Used in the map
+class lock_entry {
+private:
+  bool acquired;
+  pthread_mutex_t _m;	// mutex variable
+  pthread_cond_t  _cv;	// cond variable
+
+public:
+  lock_entry();
+  ~lock_entry();
+
+  void acquire();
+  void release();
+};
+
+
 class lock_server {
 
  protected:
   int nacquire;
-
+  // Use this data structure to manage locks
+  std::map<lock_protocol::lockid_t, lock_entry> locks;
  public:
   lock_server();
   ~lock_server() {};
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  // These were added by me to implement lock server
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 };
 
 #endif 
